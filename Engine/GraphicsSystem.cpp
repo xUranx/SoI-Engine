e5fd2f7a36b48faedac0ab5efdc3c3c51f0435b2
xUranx <samilappalainen1@kamk.fi>
2018-01-16 15:38:37 +0200

    Worked on GraphicsSystem

--- Engine/GraphicsSystem.cpp
+++ Engine/GraphicsSystem.cpp
@@ -1,6 +1,27 @@
 #include "GraphicsSystem.h"
-
+#include "Log.h"
 namespace Engine
 {
-
+	void GraphicsSystem::init(GSystem system)
+	{
+		m_system = system;
+		switch (system)
+		{
+		case GL:
+			GLenum test = glewInit();
+			if (test != GLEW_OK)
+			{
+				fatal_error("Glew Init error");
+			}
+			m_sBatch.init();
+			break;
+		case GLES20:
+			m_sBatch.init();
+		case DX11:
+			/*std::unique_ptr<SpriteBatch> spriteBatch;
+			spriteBatch = std::make_unique<SpriteBatch>(deviceContext);*/
+		default:
+			break;
+		}
+	}
 }
--- Engine/GraphicsSystem.h
+++ Engine/GraphicsSystem.h
@@ -1,6 +1,10 @@
 #pragma once
+#include<glm\glm.hpp>
+#include"Include\SpriteBatch.h"
 namespace Engine
 {
+
+
 	enum GSystem
 	{
 		GLES20, GL, DX11
@@ -8,10 +12,31 @@ namespace Engine
 	class GraphicsSystem
 	{
 	public:
-		void init(GSystem system);
-		void draw();
-	private:
 		
+		void init(GSystem system);
+
+		void draw(const glm::vec4& destRect, const glm::vec4& uvRect, GLuint texture, float depth, const ColourRGBA8& colour);
+
+		void draw(const glm::vec4& destRect, const glm::vec4& uvRect, GLuint texture, float depth, const ColourRGBA8& colour, float angle);
+
+		void draw(const glm::vec4& destRect, const glm::vec4& uvRect, GLuint texture, float depth, const ColourRGBA8& colour, const glm::vec2& dir);
+
+		void draw(const glm::vec2 destRect[], const glm::vec4& uvRect, GLuint texture, float depth, const ColourRGBA8& colour);
+
+		void draw(const glm::vec2 destRect[], const glm::vec4& uvRect, GLuint texture, float depth, const ColourRGBA8& colour, float angle, glm::vec2 pivot);
+
+		//Start Draw
+		void begin();
+
+		//End Draw
+		void end();
+
+		//Render objects
+		void render();
+
+	private:
+		SpriteBatch m_sBatch;
+		GSystem m_system;
 	};
 }
 
